ebfb9f068e96a3ac205eca812e5c03085b0eee52
Waiyaki <muturijames3@gmail.com>
2015-01-19 22:59:09 +0300

    Added login/logout views, tuned css

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,3 +103,5 @@ try:
 
 except ImportError:
     pass
+
+LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     url(r'', include('blog.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )

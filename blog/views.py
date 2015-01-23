49f5a7b2a77c959b96bf77c6800bcf96cf454930
Waiyaki <muturijames3@gmail.com>
2015-01-23 09:15:56 +0300

    Fixed a few interface bugs. Handled the case where there were no drafts or no posts. Moved the css to a separate file.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+
 from .models import Post
 from .forms import PostForm
 
